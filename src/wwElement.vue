21c9f506cf46ec18215d689827ecf5e8f3706e79
Jason Poulin <55339225+jasonvanguard@users.noreply.github.com>
2023-08-22 18:32:29 -0400

    Added customizability

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,32 +1,42 @@
 <template>
-    <component>
-        <div class="skeleton-loader"></div>
-    </component>
-</template>
-<script>
-export default {
-    name: "SkeletonLoader",
-};
-</script>
-
-<style>
-.skeleton-loader {
-    width: inherit;
-    height: inherit;
-    background-color: #c0c0c0;
-    border-radius: 50%;
-    animation: pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite;
-}
-
-@keyframes pulse {
-
+    <div :style="loaderStyle" class="skeletonLoader"></div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      content: { type: Object, required: true },
+    },
+    computed: {
+      loaderStyle() {
+        return {
+          width: this.content.loaderWidth,
+          height: this.content.loaderHeight,
+          borderRadius: this.content.borderRadius,
+          backgroundColor: this.content.backgroundColor,
+          borderRadius: this.content.borderRadius 
+        };
+      },
+    },
+  
+  };
+  </script>
+  
+  <style scoped>
+  .skeletonLoader {
+    animation: pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  }
+  
+  @keyframes pulse {
+  
     0%,
     100% {
-        opacity: 1;
+      opacity: 1;
     }
-
+  
     50% {
-        opacity: 0.5;
+      opacity: 0.5;
     }
-}
-</style>
+  }
+  </style>
+  
--- ww-config.js
+++ ww-config.js
@@ -1,11 +1,58 @@
-
 export default {
     editor: {
         label: {
-            en: 'Skeleton Loader'
+            en: "Skeleton Loader",
         },
-        icon: 'icon-lightning-bolt',
+        icon: "cursor-click",
+    },
+    properties: {
+        loaderWidth: {
+            label: {
+                en: "Loader Width",
+            },
+            type: "Length",
+            options: {
+                unitChoices: [
+                    { value: "px", label: "px" },
+                    { value: "%", label: "%" },
+                ],
+            },
+            responsive: true,
+            defaultValue: "300px",
+        },
+        loaderHeight: {
+            label: {
+                en: "Loader Height",
+            },
+            type: "Length",
+            options: {
+                unitChoices: [
+                    { value: "px", label: "px" },
+                    { value: "%", label: "%" },
+                ],
+            },
+            responsive: true,
+            defaultValue: "30px",
+        },
+        borderRadius: {
+            label: {
+                en: "Corner Roundness",
+            },
+            type: "Length",
+            options: {
+                unitChoices: [
+                    { value: "px", label: "px" },
+                    { value: "%", label: "%" },
+                ],
+            },
+            defaultValue: "5px",
+        },
+        backgroundColor: {
+            label: {
+                en: "Loader Color",
+            },
+            type: "Color",
+            defaultValue: "#c0c0c0",
+        },
     },
 };
-
-
