246c852430348160d3ba5a3aa2b88325446f5b11
Jason <jason@vanguard.to>
2023-08-23 11:43:17 -0400

    Removed duplicated line

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,42 +1,41 @@
 <template>
-    <div :style="loaderStyle" class="skeletonLoader"></div>
-  </template>
+  <div :style="loaderStyle" class="skeletonLoader"></div>
+</template>
   
-  <script>
-  export default {
-    props: {
-      content: { type: Object, required: true },
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {
+    loaderStyle() {
+      return {
+        width: this.content.loaderWidth,
+        height: this.content.loaderHeight,
+        borderRadius: this.content.borderRadius,
+        backgroundColor: this.content.backgroundColor,
+      };
     },
-    computed: {
-      loaderStyle() {
-        return {
-          width: this.content.loaderWidth,
-          height: this.content.loaderHeight,
-          borderRadius: this.content.borderRadius,
-          backgroundColor: this.content.backgroundColor,
-          borderRadius: this.content.borderRadius 
-        };
-      },
-    },
+  },
+
+};
+</script>
   
-  };
-  </script>
-  
-  <style scoped>
-  .skeletonLoader {
-    animation: pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+<style scoped>
+.skeletonLoader {
+  animation: pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+}
+
+@keyframes pulse {
+
+  0%,
+  100% {
+    opacity: 1;
   }
-  
-  @keyframes pulse {
-  
-    0%,
-    100% {
-      opacity: 1;
-    }
-  
-    50% {
-      opacity: 0.5;
-    }
+
+  50% {
+    opacity: 0.5;
   }
-  </style>
+}
+</style>
   
